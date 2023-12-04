429a77f22bf6186fad3a4ec96d3db67574c22d29
Sean B <seanb4@hotmail.co.uk>
2023-12-04 09:40:09 +0000

    version 1 complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
+const { render } = require('../app');
 var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages: messages });
 });
+
+// GET new message form
+router.get('/new', (req, res, next) => {
+  res.render("form", { title: 'New Message'})
+})
+
+router.post('/new', (req, res, next) => {
+  messages.push({text: req.body.message, user: req.body.author, added: new Date()})
+  res.redirect('/')
+})
 
 module.exports = router;
